63b7fb43d885c1a3f210774ec24fc9fa1bb38b40
Gustavo Tolay <jeremiastolay1714@gmail.com>
2024-03-12 21:25:53 -0300

    feat(pages): remake index page to work with Vue

---
+++ src/pages/_vue-views/HomePage.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ItemList from '../../components/ItemList.vue';
+import SearchBar from '../../components/SearchBar.vue';
+
+import { ref } from 'vue';
+
+const searchValue = ref('');
+</script>
+
+<template>
+  <div>
+    <div class="header">
+      <div class="container">
+        <SearchBar v-model="searchValue" />
+      </div>
+    </div>
+
+    <div class="container">
+      <ItemList :search-value />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  position: sticky;
+  top: 56px;
+  /* Navbar's height */
+  z-index: 999;
+  background-color: #13171f;
+}
+</style>
